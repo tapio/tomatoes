fa25b8467130eac152c0509a0f138c43c378e8a8
Tapio Vierros <tapiovierros@gmail.com>
2014-01-13 22:34:20 +0200

    Make resizing window preserve aspect ratio.

--- js/renderer.js
+++ js/renderer.js
@@ -1,24 +1,19 @@
 "use strict";
 
 TOMATO.RenderSystem = function() {
-	var aspect = window.innerWidth / window.innerHeight;
-	var vertHalfFov = 20;
-	var horizHalfFov = vertHalfFov * aspect;
-
-	//camera = new THREE.PerspectiveCamera(60, aspect, 0.1, 150);
-	this.camera = new THREE.OrthographicCamera(-horizHalfFov, horizHalfFov, vertHalfFov, -vertHalfFov, 0.1, 150);
-	this.camera.position.set(0, 0, 50);
-
 	this.renderer = new THREE.WebGLRenderer({
 		antialias: true,
 		preserveDrawingBuffer: true
 	});
 	this.renderer.setClearColor(0x000000);
 	this.renderer.setSize(window.innerWidth, window.innerHeight);
-	this.renderer.shadowMapDebug = false;
 	this.renderer.gammaInput = true;
 	this.renderer.gammaOutput = true;
 	this.renderer.autoClear = false;
+
+	this.camera = new THREE.OrthographicCamera(0, 0, 0, 0, 0.1, 100);
+	this.camera.position.set(10, 10, 10);
+	this.onWindowResize();
 
 	this.scene = new THREE.Scene();
 };
@@ -30,8 +25,13 @@ TOMATO.RenderSystem.prototype.render = function(dt) {
 };
 
 TOMATO.RenderSystem.prototype.onWindowResize = function() {
-	var scale = 1.0;
-	this.camera.aspect = window.innerWidth / window.innerHeight;
+	var aspect = window.innerWidth / window.innerHeight;
+	var vertHalfFov = 12;
+	var horizHalfFov = vertHalfFov * aspect;
+	this.camera.left = -horizHalfFov;
+	this.camera.right = horizHalfFov;
+	this.camera.top = vertHalfFov;
+	this.camera.bottom = -vertHalfFov;
 	this.camera.updateProjectionMatrix();
-	this.renderer.setSize(window.innerWidth * scale, window.innerHeight * scale);
+	this.renderer.setSize(window.innerWidth, window.innerHeight);
 };
