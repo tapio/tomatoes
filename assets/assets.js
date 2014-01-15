148f5c97ed55ff403b55f60bba5e276ea5a0a61d
Tapio Vierros <tapiovierros@gmail.com>
2014-01-15 02:16:14 +0200

    Draft asset data format.

--- assets/assets.js
+++ assets/assets.js
@@ -1,4 +1,25 @@
 var assets = {
-
-
+	blocks: {
+		"bridge": { sprite: "bridge.png", collision: "box" },
+		"grass": { sprite: "grass.png", collision: "box", platform: true },
+		"ladder": { sprite: "ladder.png", ladder: true },
+		"water": { sprite: "water.png", water: true }
+	},
+	clutters: {
+		"bush-small": { size: {x:1,y:1}, sprite: "bush.png" },
+		"bush-large": { size: {x:2,y:1.5}, sprite: "bush.png" },
+		"cactus": { size: {x:1.5,y:2}, sprite: "cactus.png" },
+		"plant-tiny": { size: {x:1,y:1}, sprite: "plant.png" },
+		"mushroom1": { size: {x:1,y:1}, sprite: "mushroom-brown.png" },
+		"mushroom2": { size: {x:1,y:1}, sprite: "mushroom-red.png" }
+	},
+	objects: {
+		"box": { size: {x:0.8,y:0.8}, sprite: "box.png", collision: "box", mass: 10 }
+	},
+	characters: [
+		{ size: {x:0.8,y:0.8}, sprite: "red.png" },
+		{ size: {x:0.8,y:0.8}, sprite: "green.png" },
+		{ size: {x:0.8,y:0.8}, sprite: "yellow.png" },
+		{ size: {x:0.8,y:0.8}, sprite: "blue.png" }
+	]
 };
