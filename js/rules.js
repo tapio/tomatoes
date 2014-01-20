294b3d8f8d319ffb7fcda698db011f63eca43d39
Tapio Vierros <tapiovierros@gmail.com>
2014-01-20 18:27:15 +0200

    End screen.

--- js/rules.js
+++ js/rules.js
@@ -6,29 +6,44 @@ TOMATO.RulesSystem = function() {
 };
 
 TOMATO.RulesSystem.prototype.add = function(player) {
-	this.players.push(player);
 	var div = document.createElement("div");
 	div.className = "points-row";
+
 	var icon = document.createElement("img");
 	icon.className = "icon";
 	icon.src = player.visual.mesh.material.map.sourceFile;
 	div.appendChild(icon);
+
 	var points = document.createElement("span");
 	points.className = "points";
 	div.appendChild(points);
+
 	player.domPoints = points;
 	this.container.appendChild(div);
+	this.players.push(player);
 };
 
 TOMATO.RulesSystem.prototype.update = function(dt) {
 	var playersAlive = 0;
+	var winner = -1;
 	for (var i = 0; i < this.players.length; ++i) {
 		var pl = this.players[i];
 		var status = pl.status;
 		pl.domPoints.innerHTML = /*"☠" + status.deaths +*/ "❤" + status.respawns;
-		if (!status.dead) ++playersAlive;
+		if (!status.dead) {
+			++playersAlive;
+			winner = i;
+		}
 	}
-	if (playersAlive <= 1) {
+	if (playersAlive <= 1 && TOMATO.game.running) {
 		TOMATO.game.running = false;
+		this.endScreen(winner);
 	}
+};
+
+TOMATO.RulesSystem.prototype.endScreen = function(winner) {
+	var title = (winner >= 0) ? "Player " + winner + " Wins!" : "Draw!";
+	document.getElementById("winner").innerHTML = title;
+	document.getElementById("results").className = "";
+	document.getElementById("restart").addEventListener("click", function() { window.location.reload(); });
 };
