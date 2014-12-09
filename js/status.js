579887261bda40e2f7db6ee45c5efe42a5817119
Tapio Vierros <tapiovierros@gmail.com>
2014-12-09 21:50:12 +0200

    Crappy rope swinging.

--- js/status.js
+++ js/status.js
@@ -10,6 +10,8 @@ TOMATO.Status = function(entity) {
 	this.respawns = 2;
 	this.lifeTime = Infinity;
 	this.powerUp = null;
+	this.attachedRope = null;
+	this.attachedConstraint = null;
 	this.deaths = 0;
 	this.dir = 0;
 };
@@ -20,6 +22,7 @@ TOMATO.Status.prototype.update = function(dt) {
 	this.lifeTime -= dt;
 	var entity = this.entity;
 	var controller = entity.controller;
+	var physics = TOMATO.game.physicsSystem;
 
 	var pos = entity.getPosition();
 
@@ -29,17 +32,18 @@ TOMATO.Status.prototype.update = function(dt) {
 		return;
 	}
 
-	// Update airborne and ladder status
 	var body = entity.body;
 	if (body && body.tracked) {
+		// Update ladder status
 		this.canClimb = false;
 		var ladders = TOMATO.game.world.ladders;
 		for (var i = 0, l = ladders.length; i < l; ++i) {
-			if (TOMATO.game.physicsSystem.overlaps(body, ladders[i].body)) {
+			if (physics.overlaps(body, ladders[i].body)) {
 				this.canClimb = true;
 				break;
 			}
 		}
+		// Update airborne status and jumping
 		this.airborne = !body.standing && !this.canClimb;
 		if (this.airborne) {
 			if (controller.jumpInput <= 0)
@@ -47,7 +51,26 @@ TOMATO.Status.prototype.update = function(dt) {
 			else
 				this.jump -= dt;
 		} else
-			this.jump = 0.400;
+			this.jump = 0.400; // Max jump time
+		// Update rope status
+		if ((!this.airborne || controller.jumpInput < 0.1) && this.attachedRope) {
+			physics.world.enableBodyCollision(body, this.attachedRope);
+			physics.removeConstraint(this.attachedConstraint);
+			this.attachedRope = null;
+			this.attachedConstraint = null;
+		} else if (controller.jumpInput > 0.1 && !this.attachedRope) {
+			var ropes = TOMATO.game.world.ropes;
+			for (var i = 0, l = ropes.length; i < l; ++i) {
+				var rope = ropes[i].body;
+				if (physics.overlaps(body, rope)) {
+					physics.world.disableBodyCollision(body, rope);
+					this.attachedConstraint = physics.addConstraint(body, rope, { type: "lock" });
+					this.attachedRope = rope;
+					this.jump = 0;
+					break;
+				}
+			}
+		}
 	}
 	if (body && Math.abs(body.velocity[0]) > 0.1)
 		this.dir = Math.sign(body.velocity[0]);
@@ -59,7 +82,7 @@ TOMATO.Status.prototype.update = function(dt) {
 			if (this.punchCharge > 1) this.punchCharge = 1;
 		} else if (this.punchCharge > 0) {
 			var x = body.position[0] + 0.6 * this.dir;
-			var targetBodies = TOMATO.game.physicsSystem.findBodies(x, body.position[1]);
+			var targetBodies = physics.findBodies(x, body.position[1]);
 			if (targetBodies.length) {
 				var force = [1e5 * this.punchCharge * this.dir, 1e4 * this.punchCharge];
 				for (var i = 0, l = targetBodies.length; i < l; ++i) {
